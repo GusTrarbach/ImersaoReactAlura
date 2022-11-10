4d3b7b8af3b3c6948bd51e86db0cc11d2351f730
Gustavo Trarbach <gustavo.sampaio@timenow.com.br>
2022-11-10 14:00:39 -0300

    build: fim 2a aula

---
+++ pages/index.js
@@ -0,0 +1,98 @@
+import config from "../config.json"
+import styled from "styled-components"
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu/Index";
+import { StyledTimeline } from "../src/components/Timeline";
+import React from "react";
+
+function HomePage() {
+    const [valorDoFiltro, setValorDoFiltro] = React.useState("");
+
+    return (
+        <>
+            <CSSReset />
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+            }}>
+                <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
+                <Header />
+                <Timeline searchValue={valorDoFiltro} playlists={config.playlists} />
+            </div>
+        </>
+    );
+}
+
+export default HomePage
+
+
+
+const StyledHeader = styled.div`
+    img{
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+    .user-info{
+        margin-top: 50px;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
+`;
+
+
+function Header() {
+    return (
+        <StyledHeader>
+            <section className="user-info">
+                <img src={`https://github.com/${config.github}.png`} />
+                <div>
+                    <h2>{config.name}</h2>
+                    <p>{config.job}</p>
+                </div>
+            </section>
+        </StyledHeader>
+    );
+}
+
+function Timeline({ searchValue, ...props }) {
+    const playlistNames = Object.keys(config.playlists);
+    //Statement
+    //Retorno por expressão
+    return (
+        <StyledTimeline>
+            {playlistNames.map((playlistName) => {
+                const videos = props.playlists[playlistName];
+
+                return (
+                    <section key={playlistName}>
+                        <h2>{playlistName}</h2>
+                        <div>
+                            {videos
+                                .filter((video) => {
+                                    const titleNormalized = video.title.toLowerCase();
+                                    const searchValueNormalized = searchValue.toLowerCase();
+
+                                    return titleNormalized.includes(searchValueNormalized);
+                                })
+                                .map((video) => {
+                                    return (
+                                        <a href={video.url} key={video.url}>
+                                            <img src={video.thumb} />
+                                            <span>
+                                                {video.title}
+                                            </span>
+                                        </a>
+                                    )
+                                })}
+                        </div>
+                    </section>
+                )
+            })}
+        </StyledTimeline>
+    );
+}
